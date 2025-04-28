5b8141dfd44bacea6702a990df7c2db8312def0b
Komeil <111750920+Komeyl011@users.noreply.github.com>
2025-04-28 09:49:13 +0330

    Delete .bat

--- create_package_d.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set /p target=Enter the version: 
-echo Building "GoogleDriveUploader.prerelease.v%target%" ...
-pyinstaller --onefile --clean ^
-    --name="GoogleDriveUploader.prerelease.v%target%" --add-data="./credentials.json;." --add-data="./drive_uploader.png;." ^
-    --add-data="C:\Users\Komeil\AppData\Roaming\Python\Python312\site-packages\certifi\cacert.pem;certifi" ^
-    --icon="./drive_uploader.ico" main.py
-pause
